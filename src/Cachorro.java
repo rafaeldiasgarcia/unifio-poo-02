350577a43fa79fc4cd281ca24d33f4b299c692f7
rafaeldiasgarcia <rafdgar@gmail.com>
2026-02-19 20:40:57 -0300

    first commit

---
+++ src/Cachorro.java
@@ -0,0 +1,13 @@
+public class Cachorro {
+    public String nome;
+    public int dtNasc; 
+    public String cor;
+
+    public void latir() {
+        System.out.println("Latindo");
+    }
+
+    public void correr() {
+        System.out.println("Correndo");
+    }
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,9 @@
+public class Pessoa {
+    public String nome;
+    public int telefone;
+    public String endereco;
+
+    public void pagarBoleto() {
+        System.out.println("Eu amo pagar boletos");
+    }
+}
